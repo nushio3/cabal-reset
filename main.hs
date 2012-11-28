86bbf0cb3e83e12ad7d2ee54ade44df5cf37d8a9
Takayuki Muranushi <muranushi@gmail.com>
2012-11-28 20:32:21 +0900

    better naming.

--- main.hs
+++ main.hs
@@ -1,3 +1,21 @@
+{-# LANGUAGE MultiWayIf #-}
+import System.Environment
 import System.Process
+
 main :: IO ()
-main = mapM_ system ["rm -rf ~/.ghc", "ghc-pkg recache --user"]
+main = do
+  argv <- getArgs
+  if | argv == ["of", "God"] -> go
+     | otherwise             -> usage
+
+usage :: IO ()
+usage = mapM_ putStrLn
+        [ "Usage: Wrath of God (2)(λ)(λ)"
+        , "Destroy all cabals. They can be reinstalled."
+        ]
+
+go :: IO ()
+go = mapM_ system
+     [ "rm -rf ~/.ghc"
+     , "ghc-pkg recache --user"
+     ]
